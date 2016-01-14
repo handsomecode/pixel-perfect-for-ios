b6ae70d453f2f9206cb0bfdbf92ee86fd93f0297
Anton Rozhkov <aurozhkov@gmail.com>
2016-01-14 13:33:45 +0600

    Add fabric integration

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -13,14 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
     @IBAction func ppPressed(sender: AnyObject) {
         if PixelPerfectController.isShown() {
